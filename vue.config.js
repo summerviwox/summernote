3b0ee9411c713e61314496859105ed57569ca887
summerviwox <summerviwox@gmail.com>
2022-03-16 15:21:53 +0800

    webpack-bundle-analyzer 打包文件分析工具

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpackBundleAnalyzer = require("webpack-bundle-analyzer");
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    publicPath:"/",
+    assetsDir:"assets",
+    outputDir:process.env.VUE_APP_DIST,
+    chainWebpack:config => {
+        config.plugin("webpack-bundle-analyzer")
+            .use(new webpackBundleAnalyzer.BundleAnalyzerPlugin({
+                analyzerPort: 9999
+            }))
+    }
 })
